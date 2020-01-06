b19142fe527cbd6bc2abe09079830fcb6454bc14
Vahid <vahid.ganjalizadeh@gmail.com>
2020-01-06 13:33:30 -0800

    added skewed version of mmi_gaussian wavelet to fix side peaks overlap with actual peaks and also gives more flexibility of tuning wavelet shape

--- wavelets.py
+++ wavelets.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import erf
 
 def mexican_hat(scale, window=1, dt=1):
     resolution = scale/dt
@@ -28,7 +29,7 @@ def morlet(scale, N, window=1, is_complex=False, dt=1):
 
 def mmi_gaussian(scale, N, window=1, weight=1, mod=0.5, shift=1, dt=1):
     resolution = scale/dt
-    sigma = resolution/4*mod
+    sigma = resolution/4.29193*mod
     if (type(N) == list):
         if ((type(weight) == list) and (len(N) != len(weight))):
             print('weight and N lists should have equal length')
@@ -50,6 +51,48 @@ def mmi_gaussian(scale, N, window=1, weight=1, mod=0.5, shift=1, dt=1):
             s += weight*np.exp(-(t-length/2+((N_max-1)/2-m)*resolution)**2/(2*(sigma)**2))
     s -= np.amax(s)/window*np.exp(-(t-length/2+(N_max/2+shift/2)*resolution)**2/(2*(N_max/2*window*sigma)**2))
     s -= np.amax(s)/window*np.exp(-(t-length/2-(N_max/2+shift/2)*resolution)**2/(2*(N_max/2*window*sigma)**2))
+    s -= np.mean(s)
+    s_square_norm = np.trapz(s**2, dx=dt)
+    s = s/np.sqrt(s_square_norm)
+    return s
+
+def skew_normal(x, mu, sigma, alpha=0):
+    # mean = mu - sigma*alpha/np.sqrt(1+alpha**2)*np.sqrt(2/np.pi)
+    delta = alpha/(np.sqrt(1+alpha**2))
+    mu_z = np.sqrt(2/np.pi)*delta
+    sigma_z = np.sqrt(1-mu_z**2)
+    gamma_1 = (4-np.pi)/2*(delta*np.sqrt(2/np.pi))**3/((1-2*delta**2/np.pi)**(3/2))
+    if alpha == 0:
+        m_0 = 0
+    else:
+        m_0 = mu_z - gamma_1*sigma_z/2 - np.sign(alpha)/2*np.exp(-2*np.pi/np.abs(alpha))
+    mode = mu + sigma*m_0
+    zeta = mu - sigma*m_0
+    phi = 1/np.sqrt(2*np.pi)*np.exp(-((x-zeta)**2)/(2*sigma**2))
+    _PHI = 1/2*(1+erf(alpha*(x-zeta)/sigma/np.sqrt(2)))
+    return 2/sigma*phi*_PHI
+
+def mmi_gaussian_skewed(scale, N, window=1, weight=1, mod=0.5, shift=1, skewness=1, dt=1):
+    if (type(N) != list):
+        N = [N]
+    if ((type(weight) == float) or (type(weight) == int)):
+        weight = [weight]*len(N)
+    elif (len(weight) != len(N)):
+        print('weight should be number of list (only if N is a list)')
+        return
+    N_max = max(N)
+    skewness *= 1.2*mod
+    resolution = scale/dt
+    sigma = resolution/4.29193*mod
+    # sigma = resolution/4*mod
+    length = int(N_max*resolution + shift*resolution + (1+4*skewness)*5*N_max*window*sigma)
+    t = np.arange(length)
+    s = np.zeros((length,))
+    for i,n in enumerate(N):
+        for m in range(n):
+            s += weight[i]*skew_normal(t,length/2+((n-1)/2-m)*(N_max/n)*resolution,sigma*N_max/n, alpha=0)
+    s -= N_max/2*skew_normal(t, length/2+(N_max/2+shift/2)*resolution, (1+4*skewness)*N_max/2*window*sigma, alpha=4*skewness*N_max/2*window)
+    s -= N_max/2*skew_normal(t, length/2-(N_max/2+shift/2)*resolution, (1+4*skewness)*N_max/2*window*sigma, alpha=-4*skewness*N_max/2*window)
     s -= np.mean(s)
     s_square_norm = np.trapz(s**2, dx=dt)
     s = s/np.sqrt(s_square_norm)
