829573fa8f4d3d0bebeed9f745e835070251f8b6
vganjali <vahid.ganjalizadeh@gmail.com>
2020-06-16 18:32:16 -0700

    modified Morlet envelope to include more peaks

--- wavelets.py
+++ wavelets.py
@@ -14,7 +14,7 @@ def mexican_hat(scale, window=1, dt=1):
 
 def morlet(scale, N, window=1, is_complex=False, dt=1):
     resolution = scale/dt
-    length = int(2*(N+2)*window*resolution)
+    length = int(2*(N+4)*window*resolution)
     t = np.arange(length)
     sigma = length/(10*window)
     s_exp = np.exp(-(t-length/2)**2/(2*sigma**2))
@@ -45,12 +45,16 @@ def mmi_gaussian(scale, N, window=1, weight=1, mod=0.5, shift=1, dt=1):
     if (type(N) == list):
         for i,n in enumerate(N):
             for m in range(n):
-                s += weight[i]*np.exp(-(t-length/2+((n-1)/2-m)*(N_max/n)*resolution)**2/(2*(sigma*N_max/n)**2))
+                s += weight[i]*np.exp(
+                    -(t-length/2+((n-1)/2-m)*(N_max/n)*resolution)**2/(2*(sigma*N_max/n)**2))
     else:
         for m in range(N):
-            s += weight*np.exp(-(t-length/2+((N_max-1)/2-m)*resolution)**2/(2*(sigma)**2))
-    s -= np.amax(s)/window*np.exp(-(t-length/2+(N_max/2+shift/2)*resolution)**2/(2*(N_max/2*window*sigma)**2))
-    s -= np.amax(s)/window*np.exp(-(t-length/2-(N_max/2+shift/2)*resolution)**2/(2*(N_max/2*window*sigma)**2))
+            s += weight*np.exp(
+                -(t-length/2+((N_max-1)/2-m)*resolution)**2/(2*(sigma)**2))
+    s -= np.amax(s)/window*np.exp(
+        -(t-length/2+(N_max/2+shift/2)*resolution)**2/(2*(N_max/2*window*sigma)**2))
+    s -= np.amax(s)/window*np.exp(
+        -(t-length/2-(N_max/2+shift/2)*resolution)**2/(2*(N_max/2*window*sigma)**2))
     s -= np.mean(s)
     s_square_norm = np.trapz(s**2, dx=dt)
     s = s/np.sqrt(s_square_norm)
@@ -90,9 +94,21 @@ def mmi_gaussian_skewed(scale, N, window=1, weight=1, mod=0.5, shift=1, skewness
     s = np.zeros((length,))
     for i,n in enumerate(N):
         for m in range(n):
-            s += weight[i]*skew_normal(t,length/2+((n-1)/2-m)*(N_max/n)*resolution,sigma*N_max/n, alpha=0)
-    s -= N_max/2*skew_normal(t, length/2+(N_max/2+shift/2)*resolution, (1+4*skewness)*N_max/2*window*sigma, alpha=4*skewness*N_max/2*window)
-    s -= N_max/2*skew_normal(t, length/2-(N_max/2+shift/2)*resolution, (1+4*skewness)*N_max/2*window*sigma, alpha=-4*skewness*N_max/2*window)
+            s += weight[i]*skew_normal(
+                t,
+                length/2+((n-1)/2-m)*(N_max/n)*resolution,
+                sigma*N_max/n,
+                alpha=0)
+    s -= N_max/2*skew_normal(
+        t,
+        length/2+(N_max/2+shift/2)*resolution,
+        (1+4*skewness)*N_max/2*window*sigma,
+        alpha=4*skewness*N_max/2*window)
+    s -= N_max/2*skew_normal(
+        t,
+        length/2-(N_max/2+shift/2)*resolution,
+        (1+4*skewness)*N_max/2*window*sigma,
+        alpha=-4*skewness*N_max/2*window)
     s -= np.mean(s)
     s_square_norm = np.trapz(s**2, dx=dt)
     s = s/np.sqrt(s_square_norm)
